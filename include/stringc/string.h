2dda8b9a195b2a368d7a2bb846c971d8268a1be9
Anotra <Anotra9999@gmail.com>
2021-02-06 01:17:51 -0500

    added utf8_strlen and utf8_next

--- include/stringc/string.h
+++ include/stringc/string.h
@@ -2,8 +2,16 @@
 #define STRINGC_STRING_H
 
 #include <string.h>
+#include <inttypes.h>
 
 extern char *
 strdupl(const char *const src, size_t *length);
+
+extern size_t
+utf8_strlen(const uint8_t *str);
+extern size_t
+utf8_strlen_fast(const uint8_t *str);
+extern int32_t
+utf8_next(const uint8_t **str);
 
 #endif//STRINGC_STRING_H
